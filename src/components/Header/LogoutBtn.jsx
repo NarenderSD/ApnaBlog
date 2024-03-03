152174b259e15e0caae5066f347819f02ae4db5a
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-03-03 21:55:52 +0530

    components

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import {useDispatch} from 'react-redux'
-import authService from '../../appwrite/auth'
-import {logout} from '../../store/authSlice'
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth.js";
+import { logout } from "../../store/authSlice.js";
 
 function LogoutBtn() {
-    const dispatch = useDispatch()
-    const logoutHandler = () => {
-        authService.logout().then(() => {
-            dispatch(logout())
-        })
-    }
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    authService
+      .logout()
+      .then(() => {
+        dispatch(logout());
+      })
+      .catch((e) => {
+        console.log("Error While User Logout : ", e);
+      });
+  };
+
   return (
     <button
-    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-    onClick={logoutHandler}
-    >Logout</button>
-  )
+      className="inline-block px-6 py-2 duration-200 text-white font-semibold bg-red-500 hover:bg-red-600 rounded-full"
+      onClick={logoutHandler}
+    >
+      Logout
+    </button>
+  );
 }
 
-export default LogoutBtn
+export default LogoutBtn;
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import logo from "../assets/logo.png"
 
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+    <div className='w-full'>
+      <img className='w-20 h-12 object-contain mix-blend-multiply' src={logo} alt="Logo" />
+    </div>
   )
 }
 
